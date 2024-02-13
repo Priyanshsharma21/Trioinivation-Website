ff60532c7f0a27920d6ea5dc82e14ed97967f24f
Priyanshsharma21 <piyuindia4@gmail.com>
2024-02-13 12:19:54 +0530

    file folder setup 1

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from './Home'
+import Blog from './Blog'
+import Contact from './Contact'
+import About from './About'
+import OurService from './OurService'
+
+export {
+    Home,
+    Blog,
+    Contact,
+    About,
+    OurService
+}
